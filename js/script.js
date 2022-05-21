886c6dd5a8e3636c30526cda9508ef76b877a1db
Asjeus <anderson.santos.jesus@gmail.com>
2022-05-21 18:07:46 -0300

    alteração

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-alert("teste")
+
+function cadastrarNewsletter() {
+let buscar = document.getElementById("campo-buscarProduto").value 
+alert("value")
+
+
+}
+
+function mostrarMenu(){
+// Identificar o elemento menu 
+let menu = document.getElementById("menu")
+if(getComputedStyle(menu).display == "none"){
+    menu.style.display = "flex"}
+    else{ menu.style = "none"}
+
+
+
+}
+
+// Mudar visualização 
+
+
+
