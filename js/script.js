8fa39c222aee5c5dbda774ea963cb0433889c1ff
Asjeus <anderson.santos.jesus@gmail.com>
2022-05-23 16:43:28 -0300

    alterado js

--- js/script.js
+++ js/script.js
@@ -20,10 +20,36 @@ function mostrarMenu() {
 }
 
 
+$(document).ready(function(){
+
+    let slideAtual = 1
+    let listaSlides = ["banner-1", "banner-2", "banner-3"]
+
+    setInterval(mudarSlide, 4000)
+
+   function mudarSlide() {
+
+    // Remover slider
+    if (slideAtual) {
+        $("#carrossel").removeClass(listaSlides[slideAtual - 1])
+    } else {
+        $("#carrossel").removeClass(listaSlides[listaSlides.length -1])
+    }
+     
 
 
+    // Exibir o slider atual
 
+    $("#carrossel").addClass(listaSlides[slideAtual])
 
+    // Indicar qual o slider atual
+       
+    slideAtual++
 
+    if (slideAtual > 2 ){
+        slideAtual = 0
+    }
+   }
+})
 
 
